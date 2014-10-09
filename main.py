1c2d9f87417f465118fa046f7a39618f9a2a0460
Hendrik Heuer <hendrikheuer@gmail.com>
2014-10-09 22:15:01 +0300

    added calculate_sentence_vectors and compute_sentence_similiarity_matrix

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import nltk
 from gensim.models import word2vec
 import numpy as np
 
-sentence_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+sentence_detector = nltk.data.load( 'tokenizers/punkt/english.pickle' )
 
-model = word2vec.Word2Vec.load_word2vec_format('/Users/hendrikheuer/Projects/text_summary_experiments/data/vectors.bin', binary=True)
+model = word2vec.Word2Vec.load_word2vec_format( '/Users/hendrikheuer/Projects/text_summary_experiments/data/vectors.bin', binary=True )
 
-WORD2VEC_DIM = len( model["jesus"] )
+WORD2VEC_DIM = len( model["computer"] )
 
 def cosine_similarity(v1, v2):
     return np.dot( v1, v2.T ) / np.linalg.norm(v1) / np.linalg.norm(v2)
@@ -27,32 +27,55 @@ def open_json_file_and_return_sentences( filename ):
 
     return sentences
 
-sentences_lookup = {}
 
-def split_sentence_and_return_sentence_vector( doc_id, sentence ):
-    words = sentence.split(" ")
+def split_sentence_and_return_sentence_vector( sentence ):
+    words = sentence.replace('\n', ' ').split(" ")
 
     sentence_vector = np.zeros( WORD2VEC_DIM )
 
     if len( words ) <= 30:
-        
         for w in words:
-            # remove puncation and turn it into lowercase
-            w = w.lower()
-            w = "".join(l for l in w if l not in string.punctuation)
+            if w in model:
+                # remove puncation and turn it into lowercase
+                w = w.lower()
+                w = "".join(l for l in w if l not in string.punctuation)
 
-            sentence_vector += model[ w ]
+                sentence_vector += model[ w ]
             
-    sentences_lookup.setdefault( doc_id , {} )
-    sentences_lookup[ doc_id ][ hash( sentence ) ] = sentence_vector
+    return sentence_vector
 
-    print sentence_vector
+def calculate_sentence_vectors( sentences ):
+    sentences_lookup = {}
+
+    for s in sentences[0:20]:
+        sentences_lookup[ hash( s ) ] = split_sentence_and_return_sentence_vector( s )
+
+    return sentences_lookup
+
+def compute_sentence_similiarity_matrix( simple_sentences, english_sentences ):
+    simple_array = simple_sentences.values()
+    english_array = english_sentences.values()
+
+    len_simple = len( simple_array )
+    len_english = len( english_array )
+
+    similiarity_matrix = np.zeros( (len_simple, len_english) )
+
+    for i_s in xrange( len_simple ):
+        for i_e in xrange( len_english ):
+            similiarity_matrix[i_s][i_e] = cosine_similarity( simple_array[ i_e ], english_array[ i_e ]  )
+
+    return similiarity_matrix
 
 en_file = "data/en.dog.txt"
 simple_file = "data/simple.dog.txt" 
 
-#en_dog_sentences = open_json_file_and_return_sentences( en_file )
-sentences = open_json_file_and_return_sentences( simple_file )
+simple_sentences = open_json_file_and_return_sentences( simple_file )
+simple_sentence_vectors = calculate_sentence_vectors( simple_sentences )
 
-for s in sentences[0:4]:
-    split_sentence_and_return_sentence_vector( simple_file, s )
+en_sentences = open_json_file_and_return_sentences( en_file )
+en_sentence_vectors = calculate_sentence_vectors( en_sentences )
+
+similarity_matrix = compute_sentence_similiarity_matrix( simple_sentence_vectors, en_sentence_vectors )
+
+print similarity_matrix
